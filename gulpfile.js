793c080b710b9a3b0891dd3eef6c5629e88e37a5
Damian Molinski <tedeer@gmail.com>
2021-03-15 00:36:27 +0100

    Set starter v 0.1.0

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const {
+  src, dest, watch, series,
+} = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Sass task
+function scssTask() {
+  return src('app/scss/style.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+// JavaScript task
+function jsTask() {
+  return src('app/js/script.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Browsersync Tasks
+function browsersyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: '.',
+    },
+  });
+  cb();
+}
+
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch
+function watchTask() {
+  watch('*.html', browsersyncReload);
+  watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+}
+
+// Default Gult task
+exports.default = series(
+  scssTask,
+  jsTask,
+  browsersyncServe,
+  watchTask,
+);
